5a9ae46e2952cde5a153c4d3be8ac057f5035528
Pyorang <sudangi143@gmail.com>
2023-12-20 16:24:31 +0900

    Ver1.0

---
+++ Assets/Course Library/Scripts/SpawnManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPos = new Vector3 (25, 0, 0);
+    private PlayerController playerControllerScript;
+
+    private float startDelay = 2;
+    private float repeatRate = 2;
+    
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        //Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    void SpawnObstacle()
+    {
+        if (playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
+    }
+}
